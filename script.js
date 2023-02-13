dee16ffe52955cc921f0fdb9bac2214bb035bf64
jiraffe1 <63560295+jiraffe1@users.noreply.github.com>
2023-02-13 14:32:48 +0000

    s

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var canvasElement = document.getElementById("drawingCanvas");
 var canvas = canvasElement === null || canvasElement === void 0 ? void 0 : canvasElement.getContext("2d");
 var fillColour = { r: 0, g: 0, b: 0, a: 255 };
 var strokeColour = { r: 0, g: 0, b: 0, a: 255 };
+//#region drawing
 function line(x1, y1, x2, y2) {
     canvas.moveTo(x1, y1);
     canvas.lineTo(x2, y2);
@@ -18,10 +19,13 @@ function font(fontName) {
 function text(t, x, y) {
     canvas.fillText(t, x, y);
 }
-function fill() {
+function fill(red, green, blue, alpha) {
+    fillColour = { r: red, g: green, b: blue, a: alpha };
 }
-function stroke() {
+function stroke(red, green, blue, alpha) {
+    strokeColour = { r: red, g: green, b: blue, a: alpha };
 }
+//endregion
 line(50, 50, 150, 150);
 circle(200, 200, 200);
 font("15px Arial");
